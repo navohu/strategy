47382ad43b61eb97f2657b26eeb7632545790ad7
Nathalie von Huth <nathalievh95@gmail.com>
2016-09-08 12:04:50 +0100

    Adding transition to graphs

--- vis.js
+++ vis.js
@@ -20,8 +20,6 @@ var yAxis = d3.axisLeft()
     .tickSize(-width)
     .tickFormat(function(d) { return Math.round(d*10)/10 + "%"; });
 
-
-
 // An SVG element with a bottom-right origin.
 var svg = d3.select("#statistics").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -38,60 +36,37 @@ var svg = d3.select("#statistics").append("svg")
       .classed("zero", true);
 
 var updateGraph = function(iteration, barheight){
-  d3.select('#statistics').selectAll("*").remove();
+  //  d3.select('#statistics').selectAll("*").remove();
 
-  var x = d3.scaleLinear()
-    .range([barWidth / 2, width - barWidth / 2]);
+  y.domain([getMaxYDomain(barheight), getMinYDomain(barheight)]); //dynamic y-axis
 
-  var y = d3.scaleLinear()
-    .range([height, 0])
-    .domain([getMaxYDomain(barheight), getMinYDomain(barheight)]);
+  // same for yAxis but with more transform and a title
+  svg.select(".y.axis").transition().duration(300).call(yAxis);
 
-  var yAxis = d3.axisLeft()
-    .scale(y)
-    .ticks(5)
-    .tickSize(-width)
-    .tickFormat(function(d) { return Math.round(d*10)/10 + "%"; });
-
-  var svg = d3.select("#statistics").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  svg.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(0,0)")
-    .call(yAxis)
-  .selectAll("g")
-  .filter(function(value) { return !value; })
-    .classed("zero", true);
-
-  console.log("X: " + iteration + " Y: " + barheight);
   probabilities.push({x: iteration, y: barheight});
   //Width and height
-  var w = 500;
-  var h = 100;
-  var barPadding = 1;
 
-  svg.selectAll("rect")
-    .data(probabilities)
-    .enter()
-    .append("rect")
-     .attr("x", function(d, i) {
-          return i * (width / probabilities.length);  //Bar width of 20 plus 1 for padding
-      })
-     .attr("y", function(d) {
-          return y(d.y);  //Height minus data value
-      })
-     .attr("width", width / probabilities.length - barPadding)
+  var bars = svg.selectAll(".bar").data(probabilities, function(d) { return d.y; }) // (data) is an array/iterable thing, second argument is an ID generator function
+
+  bars.exit()
     .transition()
-     .attr("height", function(d){
-        return height - y(d.y);
-     });
+      .duration(300)
+    .attr("y", y(0))
+    .attr("height", height - y(0))
+    .style('fill-opacity', 1e-6)
+    .remove();
 
+  // data that needs DOM = enter() (a set/selection, not an event!)
+  bars.enter().append("rect")
+    .attr("class", "bar")
+    .attr("y", function(d) {return y(d.y);})
+    .attr("height", function(d){return height - y(d.y);});
 
-
+  // the "UPDATE" set:
+  bars.transition().duration(300).attr("x", function(d, i) {return i * (width / probabilities.length);})
+    .attr("width", width / probabilities.length - barPadding) // constant, so no callback function(d) here
+    .attr("y", function(d) { return y(d.y); })
+    .attr("height", function(d) { return height - y(d.y); }); // flip the height, because y's domain is bottom up, but SVG renders top down
 }
 
 function getMinYDomain(y){
